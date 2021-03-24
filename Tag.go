118e9992c1342da50b5b0446117ac2e83d2a8f33
caselongo <caselongo@gmail.com>
2021-03-24 23:14:59 +0100

    x

--- Tag.go
+++ Tag.go
@@ -1,42 +1,55 @@
 package activecampaign
 
 import (
+	"fmt"
+	"net/url"
+
+	a_types "github.com/leapforce-libraries/go_activecampaign/types"
 	errortools "github.com/leapforce-libraries/go_errortools"
 	go_http "github.com/leapforce-libraries/go_http"
+	go_types "github.com/leapforce-libraries/go_types"
 )
 
 type Tags struct {
 	Tags []Tag `json:"tags"`
-	//Meta     FieldValuesMeta `json:"meta"`
+	Meta Meta  `json:"meta"`
 }
 
 type Tag struct {
-	TagType          string   `json:"tagType"`
-	Tag              string   `json:"tag"`
-	Description      string   `json:"description"`
-	SubscriberCount  string   `json:"subscriber_count"`
-	CreatedDate      string   `json:"cdate"`
-	CreatedTimestamp string   `json:"created_timestamp"`
-	UpdatedTimestamp string   `json:"updated_timestamp"`
-	CreatedBy        string   `json:"created_by"`
-	UpdatedBy        string   `json:"updated_by"`
-	ID               string   `json:"id"`
-	Links            TagLinks `json:"links"`
+	TagType          string                         `json:"tagType"`
+	Tag              string                         `json:"tag"`
+	Description      *string                        `json:"description"`
+	SubscriberCount  go_types.Int64String           `json:"subscriber_count"`
+	CreatedDate      a_types.DateTimeTimezoneString `json:"cdate"`
+	CreatedTimestamp a_types.DateTimeString         `json:"created_timestamp"`
+	UpdatedTimestamp a_types.DateTimeString         `json:"updated_timestamp"`
+	CreatedBy        *go_types.Int64String          `json:"created_by"`
+	UpdatedBy        *go_types.Int64String          `json:"updated_by"`
+	ID               go_types.Int64String           `json:"id"`
+	Links            TagLinks                       `json:"links"`
 }
 
 type TagLinks struct {
 	ContactGoalTags string `json:"contactGoalTags"`
 }
 
-type GetTagsFilter struct {
-	Email *string
+type GetTagsConfig struct {
+	Search *string
 }
 
-func (service *Service) GetTags() (*Tags, *errortools.Error) {
+func (service *Service) GetTags(getTagsConfig *GetTagsConfig) (*Tags, *errortools.Error) {
+	params := url.Values{}
+
 	tags := Tags{}
 
+	if getTagsConfig != nil {
+		if getTagsConfig.Search != nil {
+			params.Add("search", *getTagsConfig.Search)
+		}
+	}
+
 	requestConfig := go_http.RequestConfig{
-		URL:           service.url("tags"),
+		URL:           service.url(fmt.Sprintf("tags?%s", params.Encode())),
 		ResponseModel: &tags,
 	}
 
