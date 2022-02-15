5a6787a4a9b2d2398ce90d6a24ea148dd0f789bd
caselongo <caselongo@gmail.com>
2022-02-15 21:16:47 +0100

    added CustomObject

---
+++ CustomObject.go
@@ -0,0 +1,73 @@
+package activecampaign
+
+import (
+	"fmt"
+	"net/http"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	go_http "github.com/leapforce-libraries/go_http"
+	go_types "github.com/leapforce-libraries/go_types"
+)
+
+const CustomObjectRecordTimestampLayout string = "2006-01-02T15:04:05+00:00"
+
+type CustomObjectRecord struct {
+	Record CustomObjectRecordRecord `json:"record"`
+}
+
+type CustomObjectRecordRecord struct {
+	SchemaID      string                          `json:"schemaId"`
+	ID            *string                         `json:"id"`
+	ExternalID    *string                         `json:"externalId"`
+	Fields        []CustomObjectRecordField       `json:"fields"`
+	Relationships CustomObjectRecordRelationships `json:"relationships"`
+}
+
+type CustomObjectRecordRelationships struct {
+	PrimaryContact []go_types.Int64String `json:"primary-contact"`
+}
+
+type CustomObjectRecordField struct {
+	ID    string      `json:"id"`
+	Value interface{} `json:"value"`
+}
+
+func (service *Service) CreateCustomObjectRecord(r *CustomObjectRecord) (*CustomObjectRecord, *errortools.Error) {
+	if r == nil {
+		return nil, errortools.ErrorMessage("CustomObjectRecord is nil")
+	}
+
+	customObjectRecord := CustomObjectRecord{}
+
+	requestConfig := go_http.RequestConfig{
+		Method:        http.MethodPost,
+		URL:           service.url(fmt.Sprintf("customObjects/records/%s", r.Record.SchemaID)),
+		BodyModel:     r,
+		ResponseModel: &customObjectRecord,
+	}
+
+	_, _, e := service.httpRequest(&requestConfig)
+	if e != nil {
+		return nil, e
+	}
+
+	return &customObjectRecord, nil
+}
+
+func (service *Service) DeleteCustomObjectRecordByExternalID(schemaID string, externalID string) *errortools.Error {
+	if externalID == "" {
+		return errortools.ErrorMessage("externalID is empty")
+	}
+
+	requestConfig := go_http.RequestConfig{
+		Method: http.MethodDelete,
+		URL:    service.url(fmt.Sprintf("customObjects/records/%s/external/%s", schemaID, externalID)),
+	}
+
+	_, _, e := service.httpRequest(&requestConfig)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
